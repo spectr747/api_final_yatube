806253f050733df3cef8173fbf8a5958e10e13a7
Alim Gubzhokov <gubzh@mail.ru>
2022-03-19 00:30:26 +0300

    Add api views Post Group Comment Follow

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,52 @@
-# TODO:  Напишите свой вариант
+from django import views
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters
+
+from .permissions import (
+    PostPermission, 
+    CommentPermission, 
+    FollowPermission
+)
+from .serializers import (
+    PostSerializer,
+    GroupSerializer,
+    CommentSerializer,
+    FollowSerializer
+)
+from posts.models import Post, Group, Follow, Comment
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, PostPermission)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (CommentPermission,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post=post_id)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, FollowPermission,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=user__username', '=following__username')
+
+    def get_queryset(self):
+        return Follow.objects.filter(following=self.request.user)
