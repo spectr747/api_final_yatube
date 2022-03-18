3673c881c7928d870a5e4ddb3e0eaaf391c92077
Alim Gubzhokov <gubzh@mail.ru>
2022-03-18 23:54:53 +0300

    Change api urls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path
+from email.mime import base
+from django.urls import path, include
+from rest_framework import routers
+from api.views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
+)
+router.register(r'groups', GroupViewSet)
+router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
