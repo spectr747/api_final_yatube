5ba6e1f7983c4bba8752146b69b27da718798145
Alim Gubzhokov <gubzh@mail.ru>
2022-03-19 00:58:00 +0300

    Add in models Group, Comment, Follow and change settings

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,7 +1,20 @@
+from tkinter import CASCADE
 from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+
+
+class Group(models.Model):
+    slug = models.SlugField(unique=True, default='-пусто-')
+    title = models.CharField(
+        max_length=200, 
+        verbose_name='Наименование группы'
+    )
+    description = models.TextField(verbose_name='Описание группы')
+
+    def __str__(self) -> str:
+        return self.title
 
 
 class Post(models.Model):
@@ -24,3 +37,32 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=CASCADE,
+        related_name='follower',
+        null=True,
+        blank=True,
+        verbose_name='Подписчик'
+    )
+    following = models.ForeignKey(
+        User, 
+        on_delete=CASCADE,
+        related_name='following',
+        null=True,
+        blank=True,
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['following', 'user'],
+                name='unique_subscript')
+        ]
+
+    def __str__(self) -> str:
+        return f'{self.user} пописан на {self.following}'
