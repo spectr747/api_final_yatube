93eecbe9b3995f2ab819ac5d7ba0edfc9b804291
Alim Gubzhokov <gubzh@mail.ru>
2022-03-19 00:15:23 +0300

    Add permissions Post Comment Follow

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework import permissions
+
+
+class PostPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SEFE_METHODS
+            or obj.author == request.user
+        )
+
+
+class CommentPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
+class FollowPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj
